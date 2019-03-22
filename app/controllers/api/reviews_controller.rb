538a408a4027f528c4d3eb3e0b9bc1573dcc2773
Niko Dyer <nikodyer44@gmail.com>
2019-03-21 20:11:37 -0600

    added controllers

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,16 +1,47 @@
 class Api::ReviewsController < ApplicationController
+  before_action :set_work
+  before_action :set_review, only: [:show, :update, :destroy]
+
   def index
+    render json: @work.reviews
   end
 
   def show
+    render json: @review
   end
 
   def create
+    review = @work.reviews.new(review_params)
+
+    if review.save
+      render json: review
+    else
+      render json: review.errors, status: 422
+    end
   end
 
   def update
+    if @review.update(review_params)
+      render json: @review
+    else
+      render json: @review.errors, status: 422
+    end
   end
 
   def destroy
+    @review.destroy
   end
+
+  private
+    def set_work
+      @work = Work.find(params[:work_id])
+    end
+
+    def set_review
+      @review = Review.find(params[:id])
+    end
+
+    def review_params
+      params.require(:review).permit(:author, :body, :star_count, :site_name, :icon, :site_url)
+    end
 end
--- app/controllers/api/works_controller.rb
+++ app/controllers/api/works_controller.rb
@@ -1,7 +1,42 @@
 class Api::WorksController < ApplicationController
+  before_action :set_work, only: [:show, :update, :destroy]
+
   def index
+    render json: Works.all
   end
 
   def show
+    render json: @work
   end
+
+  def create
+    work = Work.new(work_params)
+
+    if work.save
+      render json: work
+    else
+      render json: work.errors, status: 422
+    end
+  end
+
+  def update
+    if @work.update(work_params)
+      render json: @work
+    else
+      render json: @work.errors, status: 422
+    end
+  end
+
+  def destroy
+    @work.destroy
+  end
+
+  private 
+    def set_work
+      @work = Works.find(params[:id])
+    end
+
+    def work_params
+      params.require(:work).permit(:name, :image_url, :web_url)
+    end
 end
