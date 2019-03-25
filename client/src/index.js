3956c7c8157656f7e57f23e925d3dc8419fec329
Nathan Swift <nswiftnswift@gmail.com>
2019-03-25 17:27:24 -0600

    ⏣ updated sidebar

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import { BrowserRouter as Router } from 'react-router-dom'
-import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
     <Router>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,30 +1,50 @@
 import React from 'react'
-import { Header, Icon } from 'semantic-ui-react'
 import styled from 'styled-components'
+import { FaAngleDown } from 'react-icons/fa'
 
-const Home = () => (
-    <div style={{display: 'flex'}}>
-        <HeaderText as="h1" textAlign="center">Octopus Mountain Co</HeaderText>
-        <Header as={ScrollIcon} textAlign="center"> Scroll Down <br/>
-            <Icon name="angle down" />
-        </Header>
-    </div>
-)
 
-const HeaderText = styled(Header)`
-    display: flex !important
-    align-items: center 
-    justify-content: center 
-    width: 100%
-    height: auto
-    padding-top: 20%
+export default class Home extends React.Component {
+  state = { scrollTop: 0 }
+
+    componentDidMount() {
+      window.addEventListener('scroll', this.handleScroll)
+    }
+
+    handleScroll = () => {
+        console.log('Scrolled!')
+    }
+    
+
+    render() {
+        return (
+          <div style={{ display: 'flex !important'}}>
+            <div style={{ display: 'flex !important' }} onScroll={this.handleScroll} >
+                <HeaderText as="h1" textAlign="center">Octopus Mountain Co</HeaderText>
+            </div>
+            <div style={{ display: 'flex !important' }}> 
+              <ScrollIcon>Scroll Down
+                <FaAngleDown/>
+              </ScrollIcon>
+            </div>
+          </div>
+        )
+    }
+}
+
+const HeaderText = styled.div`
+  display: flex !important
+  align-items: center !important
+  justify-content: center !important
+  width: 100% !important
+  height: 300px !important
+  margin: auto !important
+  transform: translate(-2%, 30%)
 `
 
-const ScrollIcon = styled(Header)`
-    display: flex !important
-    justify-content: center
-    width: 100%
-    height: auto
+const ScrollIcon = styled.h3`
+  display: flex !important
+  justify-content: center !important
+  align-items: flex-end !important
+  margin-top: 20%
+  margin-right: 2%
 `
-
-export default Home
