51af60f6ae5a658894bd0471c316f53730cead65
Niko Dyer <nikodyer44@gmail.com>
2019-03-26 09:30:02 -0600

    unpushed commit

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
-import { FaAngleDown } from 'react-icons/fa'
+import { FaAngleDown, FaFacebookF, FaInstagram, FaTwitter, FaGithub, FaLinkedin } from 'react-icons/fa'
 
 
 export default class Home extends React.Component {
-  state = { height: window.pageYOffset }
+  state = {
+    height: window.pageYOffset,
+    scrolled: false, 
+    count: 0
+  }
 
   componentDidMount() {
     window.addEventListener('scroll', this.handleScroll)
@@ -12,31 +16,50 @@ export default class Home extends React.Component {
 
   handleScroll = () => {
     this.setState({ height: window.pageYOffset })
-
-    if (this.state.height > 100) {
-      this.setState({ scrolled: !this.state.scrolled })
-    }
+    if (this.state.height > 200) {
+      window.scrollTo(0, 0)
+      this.setState({ scrolled: true, count: this.state.count++ })
+    } 
+    // else if (this.state.height )
   }
+
+
+  // if !scrollTop render the next home page snippet and hide scroll down option - set to hidden to keep page position 
+  // else if the scrollTop is hitting on the initial homepage - make the scroll icon pulse
+  // if scrollTop go back to the last code snippet
 
 
   render() {
     return (
-      <div style={{ display: 'flex !important' }}>
-        {this.state.scrolled ?
-          <div>tits</div>
-          :
-          <div>
-            <div style={{ display: 'flex !important' }}>
-              <HeaderText as="h1" textAlign="center">Octopus Mountain Co</HeaderText>
+      <PageBody>
+        <div style={{ display: 'flex !important' }}>
+          {this.state.scrolled ?
+            <div>
+              <div style={{ display: 'hidden !important', position: 'fixed !important' }}>
+                <HeaderText2 as="h1" textAlign="center">Something about having peace in business choices om lalala</HeaderText2>
+              </div>
             </div>
-            <div style={{ display: 'flex !important' }}>
-              <ScrollIcon>Scroll Down
+            :
+            <div>
+              <div style={{ display: 'flex !important' }}>
+                <HeaderText as="h1" textAlign="center">Octopus Mountain Co</HeaderText>
+              </div>
+              <div style={{ display: 'flex !important' }}>
+                <ScrollIcon>Scroll Down
                 <FaAngleDown />
-              </ScrollIcon>
+                </ScrollIcon>
+              </div>
             </div>
-          </div>
-        }
-      </div>
+          }
+          <Footer style={{ display: 'flex !important' }}>
+            <FaFacebookF />
+            <FaInstagram />
+            <FaTwitter />
+            <FaGithub />
+            <FaLinkedin />
+          </Footer>
+        </div>
+      </PageBody>
     )
   }
 }
@@ -50,6 +73,16 @@ const HeaderText = styled.div`
   margin: auto !important
   transform: translate(-2%, 30%)
 `
+const HeaderText2 = styled.div`
+  display: flex !important
+  align-items: center !important
+  justify-content: center !important
+  width: 100% !important
+  height: 400px !important
+  margin: auto !important
+  margin-bottom: 200px !important
+  transform: translate(-2%, 30%)
+`
 
 const ScrollIcon = styled.h3`
   display: flex !important
@@ -57,4 +90,18 @@ const ScrollIcon = styled.h3`
   align-items: flex-end !important
   margin-top: 20%
   margin-right: 2%
+`
+
+const PageBody = styled.div`
+  width: 100%
+  height: 2000px
+`
+
+const Footer = styled.table`
+  width: 100%
+  display: flex !important
+  align-content: flex-end !important
+  justify-content: center !important
+  margin-left: -20px
+  transform: translate(0, 100px)
 `
