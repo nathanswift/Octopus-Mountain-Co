f2968fa4ffacbad921fd4dffd990f6a79dadfee2
Niko Dyer <nikodyer44@gmail.com>
2019-04-08 22:23:05 -0600

    login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import { AuthConsumer } from "../providers/AuthProvider"
 import { Link } from 'react-router-dom'
+import { Form, Segment, Header } from 'semantic-ui-react'
 import styled from 'styled-components'
+import Background from '../assets/images/AltaMap.png'
 
 class Login extends React.Component {
   state = { email: '', password: '' }
-  
+
   handleSubmit = (e) => {
     e.preventDefault()
     const { email, password } = this.state
     this.props.auth.handleLogin({ email, password }, this.props.history)
   }
-  
+
   handleChange = (e) => {
     const { name, value } = e.target
     this.setState({ [name]: value })
@@ -19,34 +21,33 @@ class Login extends React.Component {
 
   render() {
     const { email, password } = this.state
-  
+
     return (
-      <FormContainer style={{display: 'flex !important'}}>
-        <h1>Login</h1>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            label="Email"
-            autoFocus
-            required         
-            name='email'
-            value={email}
-            placeholder='Email'
-            onChange={this.handleChange}
-          />
-          <input
-            label="Password"
-            required
-            name='password'
-            value={password}
-            placeholder='Password'
-            type='password'
-            onChange={this.handleChange}
-          />
-          <SubmitButton onClick={this.handleSubmit} class="btn" type='submit'>Submit</SubmitButton>
-          <Link to='/Register'>
-            <div class="btn">Register</div>
-          </Link>
-        </form>
+      <FormContainer>
+        <Header textAlign='center' style={{color: 'white', letterSpacing: '2em', marginRight: '-1.5em', fontSize: '5em'}} as='h1'>Om</Header>
+        <Segment centered inverted style={{display: 'flex', justifyContent: 'center', marginTop: '-10em', marginRight: '10em'}} textAlign='center'>
+          <Form inverted onSubmit={this.handleSubmit}>
+            <Form.Input
+              label='Email'
+              autoFocus
+              required
+              name='email'
+              value={email}
+              placeholder='Email'
+              onChange={this.handleChange}
+              />
+            <Form.Input
+              label='Password'
+              required
+              name='password'
+              value={password}
+              placeholder='Password'
+              type='password'
+              onChange={this.handleChange}
+              />
+            <Form.Button type='submit' color='white' inverted>Log In</Form.Button>
+          </Form>
+        </Segment>
       </FormContainer>
     )
   }
@@ -56,7 +57,7 @@ export default class ConnectedLogin extends React.Component {
   render() {
     return (
       <AuthConsumer>
-        { auth => <Login {...this.props} auth={auth} />}
+        {auth => <Login {...this.props} auth={auth} />}
       </AuthConsumer>
     )
   }
@@ -64,10 +65,11 @@ export default class ConnectedLogin extends React.Component {
 
 const FormContainer = styled.div`
   display: flex !important
-  width: 40em
-  height: 45em
+  width: 100%
+  height: 70em
   justify-content: center !important
   align-items: center !important
+  background-image: url(${Background})
 `
 
 const SubmitButton = styled.div`
