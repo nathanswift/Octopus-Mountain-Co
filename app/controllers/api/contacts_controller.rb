74373cd73093231cc1741e1c8cd97c73dd5d2a0e
Ryan Gale <ryanwfgale@gmail.com>
2019-04-09 19:25:08 -0600

    add contacts controller, model and functioning contact form

---
+++ app/controllers/api/contacts_controller.rb
@@ -0,0 +1,44 @@
+class Api::ContactsController < ApplicationController
+before_action :set_contact, only: [:show, :update, :destroy]
+
+def index
+  render json: @contact.all
+end
+
+def show
+  render json: @contact
+end
+
+def create
+  contact = Contact.new(contact_params)
+
+  if contact.save
+    render json: contact
+  else
+    render json: contact.errors, status: 422
+  end
+end
+
+def update
+  if @contact.update(contact_params)
+    render json: @contact
+  else
+    render json: @contact.errors, status: 422
+  end
+end
+
+def destroy
+  @contact.destroy
+end
+
+private
+
+  def set_contact
+    @contact = Contact.find(params[:id])
+  end
+
+  def contact_params
+    params.require(:contact).permit(:email, :message)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     get 'works/index'
     get 'works/show'
     resources :users, only: :update
+    resources :contacts
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '*other', to: 'static#index'
