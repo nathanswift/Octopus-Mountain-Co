7052d2ee318eedfaca50e652c9528f4a67d2b050
Niko Dyer <nikodyer44@gmail.com>
2019-04-18 17:58:02 -0600

    animations on works

--- client/src/pages/Works.js
+++ client/src/pages/Works.js
@@ -1,53 +1,89 @@
-import React, { Fragment } from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import ScrollAnimation from 'react-animate-on-scroll'
 import Map from '../assets/images/AltaMap.png'
 import { Button, Container, Header, Icon, Image, Segment } from 'semantic-ui-react'
+import '../assets/styles/styles.css'
 
-class Works extends React.Component {
-  state = { works: [] }
+const Works = (props) => {
 
-  componentDidMount() {
-    axios.get('/api/works/index')
-      .then( res => {
-        this.setState({ works: res.data})
-      })
-      .catch( err => {
-        console.log(err)
-      })
-  }
+  const [works, setWorks] = useState([])
+  
+  // showWork() {
+  //   this.setState({ shouldShowWork: true })
+  // }
 
-  render() {
-    const { works } = this.state
+  useEffect( () => {
+    axios
+        .get('/api/works')
+        .then( res => {
+          setWorks(res.data)
+        })
+        .catch( err => {
+          console.log(err)
+        })
+  }, [])
+  
+  // componentDidMount() {
+      // axios
+      //   .get('/api/works')
+      //   .then( res => {
+      //   this.setState({ works: res.data.data })
+      // })
+      // .catch( err => {
+      //     console.log(err)
+      //   })
+  // }
+  
+  // render() {
+    // const workIcon = 
+    // <button 
+    // className='mdl-button mdl-js-button' 
+    // id='icon-work' 
+    //     // onClick={this.showWork}
+    //     >
+    //   <span 
+    //     className='glyphicon glyphicon-work' 
+    //     aria-hidden='true'
+    //     >
+    //   </span></button>
+
+
     return(
       <ContainerStyles>
         { works.map( work => (
           <Link
-            to={{ pathname: '/WorksShow', state: { ...work }}}
+            to={{ pathname: '/WorksShow' }}
           >
-            <SegmentStyles placeholder>
-              <HeaderStyles>
-                {work.name}
-              </HeaderStyles>
+            <ScrollAnimation
+              animateIn='flipInX'
+              animateOut='fadeOut'
+            >
+              <SegmentStyles placeholder className='work-enter`'>
+                <HeaderStyles>
+                  {work.name}
+                </HeaderStyles>
                 <ImageStyle fluid src={work.image_url} alt=""/>
-            </SegmentStyles>      
+              </SegmentStyles>
+            </ScrollAnimation>
           </Link>
-        ))}
+        ))
+      }
       </ContainerStyles>
     )
-  }
 }
 
 const ImageStyle = styled(Image)`
-  width: 100% !important
-  height: 30em !important
-  z-index: 0
+width: 100% !important
+height: 30em !important
+z-index: 0
 `
 const HeaderStyles = styled.span`
-  color: white !important
-  z-index: 2
-  transform: translate(0, 10em)
+color: white !important
+z-index: 2
+transform: translate(0, 10em)
 `
 const ContainerStyles = styled(Container)`
   opacity: .5
@@ -57,9 +93,8 @@ const SegmentStyles = styled(Segment)`
   width: 80%
   background: transparent !important 
   transform: translate(8em)
-
-  @keyframes 
 `
 
 
 export default Works
+
