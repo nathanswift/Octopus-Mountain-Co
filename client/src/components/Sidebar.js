2a27fd31f2384c1169a1f06d141adc8d8a8babcb
Ryan Gale <ryanwfgale@gmail.com>
2019-05-24 21:57:33 -0600

    fix sidebar navbar sp-acing

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,191 +1,261 @@
-import React from "react"
-import { Link, withRouter } from "react-router-dom"
-import styled from "styled-components"
-import ScrollLock from "react-scrolllock"
-import { AuthConsumer } from "../providers/AuthProvider"
-import MenuImg from "../assets/xd/+.svg"
-import { Image, Grid, Icon, Header, Container } from "semantic-ui-react"
-import ContactForm from "./ContactForm"
-import About from "../components/About"
-import Services from "../components/Services"
-import SidebarHome from "../components/SidebarHome"
-import Paws from '../assets/images/octopuss-glow.png'
-import blackMenuImg from "../assets/images/OmMenuLogo.png"
-import SidebarLanding from '../assets/images/bgservices.svg'
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import styled from "styled-components";
+import ScrollLock from "react-scrolllock";
+import { AuthConsumer } from "../providers/AuthProvider";
+import MenuImg from "../assets/xd/+.svg";
+import {
+  Image,
+  Grid,
+  Icon,
+  Header,
+  Container,
+  Navbar
+} from "semantic-ui-react";
+import ContactForm from "./ContactForm";
+import About from "../components/About";
+import Services from "../components/Services";
+import SidebarHome from "../components/SidebarHome";
+import Paws from "../assets/images/octopuss-glow.png";
+import blackMenuImg from "../assets/images/OmMenuLogo.png";
+import SidebarLanding from "../assets/images/bgservices.svg";
 
 class Sidebars extends React.Component {
   state = {
     visible: false,
-    sidebarText: 
-      <SidebarHome />,
-      lockScroll: false
-    }
-    
+    sidebarText: <SidebarHome />,
+    lockScroll: false
+  };
+
   handleAbout = () => {
     this.setState({
-      sidebarText: 
-        <About />,
-    })
-  }
-  
+      sidebarText: <About />
+    });
+  };
+
   handleServices = () => {
     this.setState({
-      sidebarText: 
-        <Services />,
-    })
-  }
+      sidebarText: <Services />
+    });
+  };
 
   handleContact = () =>
     this.setState({
-      sidebarText: 
-        <ContactForm toggleMenu={this.toggleMenu} />
-    })
+      sidebarText: <ContactForm toggleMenu={this.toggleMenu} />
+    });
 
   toggleMenu = () => {
     this.setState({
       visible: !this.state.visible,
       lockScroll: !this.state.lockScroll
-    })
+    });
     window.scrollTo(0, 0);
-  }
+  };
 
   onClick = () => {
-    this.toggleMenu()
-    this.props.auth.handleLogout(this.props.history)
-  }
+    this.toggleMenu();
+    this.props.auth.handleLogout(this.props.history);
+  };
 
   NavItems = () => {
     const {
       auth: { user }
-    } = this.props
+    } = this.props;
 
     if (user) {
       return (
         <LoginButton name="LogOut" onClick={this.onClick}>
           logOut
         </LoginButton>
-      )
+      );
     } else {
       return (
         <Link to="/Login">
-          <LoginButton onClick={this.toggleMenu} className="btn">
+          <LoginButton
+            onClick={this.toggleMenu}
+            className="btn"
+            style={{ color: "#141414", paddingTop: "3%" }}
+          >
             login
           </LoginButton>
         </Link>
-      )
+      );
     }
-  }
+  };
 
   render() {
     return (
       <>
-        <Sidebar style={ this.state.visible ? {display: "inline-block", background: 'white', zIndex: '3', position: 'absolute' } : {display: 'none'} }>
+        <Sidebar
+          style={
+            this.state.visible
+              ? {
+                  display: "inline-block",
+                  background: "white",
+                  zIndex: "3",
+                  position: "absolute"
+                }
+              : { display: "none" }
+          }
+        >
           {this.state.visible ? (
             <>
-              <Grid colums="equal" style={{zIndex: '3'}}>
+              <Grid colums="equal" style={{ zIndex: "3" }}>
                 <Grid.Row col={3}>
                   <Grid.Column>
                     <MenuButton
-                    onClick={this.toggleMenu}
-                    style={{ display: "flex !important", marginTop: '2%', width: '35px', height: '35px', zIndex: '4' }}
+                      onClick={this.toggleMenu}
+                      style={{
+                        display: "flex !important",
+                        marginTop: "2%",
+                        width: "35px",
+                        height: "35px",
+                        zIndex: "4"
+                      }}
                     >
                       <Image
-                      src={MenuImg}
-                      style={{ display: 'inline-block', height: "5em", width: "5em", marginLeft: "3em", position: 'absolute', zIndex: '4'}} /* Something with a sticky posititon would be cool here - have paws sliding up into a fixed spot. - Or just an animation*/
+                        src={MenuImg}
+                        style={{
+                          display: "inline-block",
+                          height: "5em",
+                          width: "5em",
+                          marginLeft: "3em",
+                          position: "absolute",
+                          zIndex: "4"
+                        }} /* Something with a sticky posititon would be cool here - have paws sliding up into a fixed spot. - Or just an animation*/
                       />
                     </MenuButton>
                   </Grid.Column>
                   <Grid.Column width={8}>
-                    <Image src={Paws} style={{width: '600px', height: 'auto', zIndex: '4', transform: 'translate(-8em, -12.5em)'}}/>
+                    <Image
+                      src={Paws}
+                      style={{
+                        width: "600px",
+                        height: "auto",
+                        zIndex: "4",
+                        transform: "translate(-8em, -12.5em)"
+                      }}
+                    />
                   </Grid.Column>
-                  <Grid.Column>
-                  <Link as={ButtonStyle} to='/HomeShow' onClick={this.toggleMenu}>
-                    home
-                  </Link>
-                  <ButtonStyle className="btn" onClick={() => this.handleAbout()}>
-                    About
-                  </ButtonStyle>
-                  <ButtonStyle
-                    className="btn"
-                    onClick={() => this.handleServices()}
+                  <Grid.Column
+                    style={{
+                      display: "flex",
+                      justifyContent: "flex-start",
+                      flexDirection: "row",
+                      marginLeft: "35%"
+                    }}
+                  >
+                    <Link
+                      as={ButtonStyle}
+                      to="/HomeShow"
+                      onClick={this.toggleMenu}
+                      style={{
+                        color: "#141414",
+                        paddingTop: "72%",
+                        paddingRight: "20%"
+                      }}
                     >
-                    services
-                  </ButtonStyle>
-                  <Link to='/Works'>
-                    <ButtonStyle onClick={this.toggleMenu}>
-                      works
+                      home
+                    </Link>
+                    <ButtonStyle
+                      className="btn"
+                      onClick={() => this.handleAbout()}
+                      style={{ paddingTop: "70%", paddingRight: "20%" }}
+                    >
+                      About
                     </ButtonStyle>
-                  </Link>
-                  <ButtonStyle onClick={() => this.handleContact()}>
-                    contAct
-                  </ButtonStyle>
-                  { this.NavItems() }
+                    <ButtonStyle
+                      className="btn"
+                      onClick={() => this.handleServices()}
+                      style={{ paddingTop: "70%", paddingRight: "20%" }}
+                    >
+                      services
+                    </ButtonStyle>
+                    <Link to="/Works">
+                      <ButtonStyle
+                        onClick={this.toggleMenu}
+                        style={{
+                          paddingTop: "43%",
+                          paddingRight: "20%",
+                          color: "#141414"
+                        }}
+                      >
+                        works
+                      </ButtonStyle>
+                    </Link>
+                    <ButtonStyle
+                      onClick={() => this.handleContact()}
+                      style={{ paddingTop: "70%", paddingRight: "10%" }}
+                    >
+                      contAct
+                    </ButtonStyle>
+                    {this.NavItems()}
                   </Grid.Column>
                 </Grid.Row>
-                <Grid style={{ display: 'inline-block', background: `url(${SidebarLanding})`, width: '100%', height: '90%', position: 'absolute'}}>
-                  <Grid.Row>
-                  { this.state.sidebarText }
+                <Grid
+                  style={{
+                    display: "inline-block",
+                    background: `url(${SidebarLanding})`,
+                    width: "100%",
+                    height: "90%",
+                    position: "absolute"
+                  }}
+                >
+                  <Grid.Row>{this.state.sidebarText}</Grid.Row>
+                  <Grid.Row col={2}>
+                    <Grid.Column width={6}>
+                      <Header> Recent Twitter Post</Header>
+                      <p>
+                        Cum sociis natoque penatibus et magnis dis parturient
+                        montes, nascetur ridiculus mus. Nullam id dolor id nibh
+                        ultricies vehicula ut id elit.
+                      </p>
+                    </Grid.Column>
+                    <Grid.Column width={6}>
+                      <Header> Recent Twitter Post</Header>
+                      <p>
+                        Cum sociis natoque penatibus et magnis dis parturient
+                        montes, nascetur ridiculus mus. Nullam id dolor id nibh
+                        ultricies vehicula ut id elit.
+                      </p>
+                    </Grid.Column>
                   </Grid.Row>
-                <Grid.Row col={2}>
-                  <Grid.Column width={6}>
-                    <Header> Recent Twitter Post</Header>
-                    <p>
-                      Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
-                      Nullam id dolor id nibh ultricies vehicula ut id elit.
-                    </p>
-                  </Grid.Column>
-                  <Grid.Column width={6}>
-                    <Header> Recent Twitter Post</Header>
-                    <p>
-                      Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
-                      Nullam id dolor id nibh ultricies vehicula ut id elit.
-                    </p>
-                  </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                  <Grid.Column width={6}>
-                    <div> CopyRight @Octopus Mountain Co. </div>
-                  </Grid.Column>
-                  <Grid.Column width={6}>
-                      <Icon 
-                        color="black"
-                        name="facebook f"
-                        size="large"
-                        />
-                      <Icon 
-                        color="black"
-                        name="twitter"
-                        size="large"
-                        />
-                      <Icon 
+                  <Grid.Row>
+                    <Grid.Column width={6}>
+                      <div> CopyRight @Octopus Mountain Co. </div>
+                    </Grid.Column>
+                    <Grid.Column width={6}>
+                      <Icon color="black" name="facebook f" size="large" />
+                      <Icon color="black" name="twitter" size="large" />
+                      <Icon
                         color="black"
                         name="github alternate"
                         size="large"
-                        />
-                      <Icon 
-                        color="black"
-                        name="linkedin square"
-                        size="large"
-                        />
-                  </Grid.Column>
-                </Grid.Row>
+                      />
+                      <Icon color="black" name="linkedin square" size="large" />
+                    </Grid.Column>
+                  </Grid.Row>
                 </Grid>
-              </Grid> 
+              </Grid>
             </>
-          ) : 
-          <MenuButton
-          onClick={this.toggleMenu}
-          style={{ display: "flex !important", zIndex: '1',  marginTop: '2%' }}
-          >
+          ) : (
+            <MenuButton
+              onClick={this.toggleMenu}
+              style={{
+                display: "flex !important",
+                zIndex: "1",
+                marginTop: "2%"
+              }}
+            >
               <Image
-              src={blackMenuImg}
-              style={{ height: "5em", width: "5em", marginLeft: "3em" }}
+                src={blackMenuImg}
+                style={{ height: "5em", width: "5em", marginLeft: "3em" }}
               />
             </MenuButton>
-          }
-          </Sidebar>
-        </>
-    )
+          )}
+        </Sidebar>
+      </>
+    );
   }
 }
 
@@ -195,7 +265,7 @@ class ConnectSidebars extends React.Component {
       <AuthConsumer>
         {value => <Sidebars {...this.props} auth={value} />}
       </AuthConsumer>
-    )
+    );
   }
 }
 
@@ -205,10 +275,10 @@ const Sidebar = styled.div`
   position: absolute !important
   width: 100%
   height: 100%
-`
+`;
 const MenuButton = styled.div`
-  position: fixed !important
-`
+  position: fixed !important;
+`;
 
 const ButtonStyle = styled.div`
   display: inline-block
@@ -216,13 +286,13 @@ const ButtonStyle = styled.div`
   margin-top: 5%
   float: left !important
   font-family: Major Mono Display, monospace; !important
-`
+`;
 const LoginButton = styled.div`
   display: flex !important
   margin-top: 2em
   margin-right: 1em
   height: 6em
   font-family: Major Mono Display, monospace; !important
-`
+`;
 
-export default withRouter(ConnectSidebars)
+export default withRouter(ConnectSidebars);
